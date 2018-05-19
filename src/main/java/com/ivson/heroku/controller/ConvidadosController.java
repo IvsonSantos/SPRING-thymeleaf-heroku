a6ed1a711989bc9cdaf192368a121e78b81b3ddd
Ivson Santos <santosivson@hotmail.com>
2018-05-19 11:29:46 -0300

    Metodo POST com atualizacao de tela

--- src/main/java/com/ivson/heroku/controller/ConvidadosController.java
+++ src/main/java/com/ivson/heroku/controller/ConvidadosController.java
@@ -3,20 +3,31 @@ package com.ivson.heroku.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.ivson.heroku.model.Convidado;
 import com.ivson.heroku.repository.ConvidadosRepository;
 
 @Controller
+@RequestMapping("/convidados")
 public class ConvidadosController {
 
 	@Autowired
 	private ConvidadosRepository convidadosRepository;
 
-	@GetMapping("/convidados")
+	@GetMapping
 	public ModelAndView listar() {
 		ModelAndView modelAndView = new ModelAndView("lista-convidados");
 		modelAndView.addObject("convidados", convidadosRepository.findAll());
+		modelAndView.addObject(new Convidado());
 		return modelAndView;
+	}
+
+	@PostMapping
+	public String salvar(Convidado convidado) {
+		this.convidadosRepository.save(convidado);
+		return "redirect:/convidados";
 	}
 }
